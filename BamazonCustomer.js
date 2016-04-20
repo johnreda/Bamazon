1bef33080a996fde3f1aa1477f99767edda0c870
John Reda <johnreda4@gmail.com>
2016-04-20 09:15:43 -0400

    FIRST COMMIT

---
+++ BamazonCustomer.js
@@ -0,0 +1,61 @@
+var mysql = require ('mysql');
+var prompt = require ('prompt');
+
+
+// CONNECTION SCHEMA
+		var con = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		database: 'Bamazon'
+		});
+
+//GLOBAL VARIABLES (???)
+
+
+	//CONNECTING TO DATABASE
+	con.connect(function (err){
+		if (err){
+			console.log(err + "NOPE");
+		}
+		console.log ('connected');
+	});
+
+		//CREATE THE QUERY	
+		con.query("SELECT * FROM Products", function (err, rows){
+			
+
+
+			if (err) {
+				return ("error");
+			} else {
+				//CREATES A LOOP TO RETURN FORMATTED DATA
+			for (var i = 0; i < rows.length; i++) {
+					console.log ("Item id: " + rows[i].ItemID +
+								 ", Product: " + rows[i].ProductName +
+								 ", Price: $" + rows[i].Price + ".00" +
+								 ", Quantity: " + rows[i].StockQuantity);
+				
+				//SETS THE TABLE VALUES AS VARIABLES TO COMPARE WITH USER'S ORDER
+				var itemId = rows[i].ItemID;
+				var product = rows[i].ProductName;
+				var price = rows[i].Price;
+				var startQuant = row[i].StockQuantity;
+
+			};
+		}
+
+		prompt.start() 
+
+			prompt.get(['productid', 'quantity'], function (err, result){
+
+				var productId = result.productid;
+					console.log(productid);
+				var quantity = result.quantity;
+					console.log(quantity);
+			})
+
+			//CHECK TO SEE IF THERE IS ENOUGH STOCK
+			con.query("SELECT * FROM Products WHERE ItemID =" + itemID, function (err, rows){
+				console.log(rows);
+			})
+	});
